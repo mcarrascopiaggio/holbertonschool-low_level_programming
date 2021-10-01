697f71810dbc73068d67fc0b01d0a811ef96c5d7
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-09-30 19:21:20 -0700

    task4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 
 /**
  * main - prints a phrase followed by a new line.
- * rerurn: 0.
+ * description: prints a phrase
+ * return: 0.
  */
-int main(void)
+int void main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
