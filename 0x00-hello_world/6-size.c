bb27cc4ff5a8e0bd1ecc3438e4854fc8e9d497c9
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-01 06:03:40 -0700

    task 6 with long long correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,14 @@ int main(void)
 {
 	int intType;
 	float floatType;
-	double doubleType;
+	long longType;
+	long long longlongType;
 	char charType;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(doubleType));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
