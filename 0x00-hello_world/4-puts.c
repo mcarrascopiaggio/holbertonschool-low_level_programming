be662e578b9d1d79bf02ad3c9c06f42c4749d8a1
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-09-30 17:51:27 -0700

    first version of task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints a phrase followed by a new line.
+ * rerurn: 0.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
