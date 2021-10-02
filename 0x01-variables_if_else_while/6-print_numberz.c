77ba02573964f8b629dbf2d058d429dc86d51915
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-01 18:50:27 -0700

    prints single numbers limit commands

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-9
+ * Description: prints single digit
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	printf("%d", i);
+	putchar ('\n');
+	return (0);
+}
