eeb6d0c157b8da0de48da275b403eb481056b31c
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-03 20:17:24 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - print the 0-9
+ * Description: prints the 0,9 using putchar
+ * Return: 0.
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	putchar (ch),
+	putchar (',');
+	putchar ('$');
+	putchar ('\n');
+	return (0);
+}
