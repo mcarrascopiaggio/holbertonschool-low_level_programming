bc939b92391a7f5263017cbb2e7066d0fb48f50f
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-05 09:34:54 -0700

    task o

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ *  * main - print the word _putchar
+ *   * print the word using the function _putchar
+ *    * Return: 0.
+ *     */
+
+int main(void)
+{
+
+	int i;
+	char PC [] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		_putchar(PC[i]);
+	}
+	_putchar('\n');
+return (0);
+}
