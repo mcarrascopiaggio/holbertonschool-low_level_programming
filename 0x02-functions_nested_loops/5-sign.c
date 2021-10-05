fa525983dd1e34b0da9d4713d022a0be54ac9be2
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-05 15:54:00 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+*print_sign -  check if a variable is lowercase
+*@n: The integer to print
+*Return: On success 1.
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+	return (1);
+	_putchar (43);
+	}
+	else if (n < 0)
+		{
+		return (-1);
+		_putchar (45);
+		_putchar (43);
+		}
+		else
+		{
+			return (0);
+			_putchar(48);
+		}
+}
+
+
