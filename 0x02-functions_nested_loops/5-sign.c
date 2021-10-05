a7f5d42419c9e3cbd9e1e15c28ea9f9fb0cb10ae
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-05 15:59:06 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,16 +15,18 @@ int print_sign(int n)
 	{
 	return (1);
 	_putchar (43);
+	_putchar (49);
 	}
 	else if (n < 0)
 		{
 		return (-1);
 		_putchar (45);
-		_putchar (43);
+		_putchar (49);
 		}
 		else
 		{
 			return (0);
+			_putchar(48);
 			_putchar(48);
 		}
 }
