f77955d8887d2c4a581f563e2f9e1f961f32a507
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-06 14:02:05 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+*add -  return the absolute number
+*@a: the first value
+*@b: the second value
+*Return: the addition of a and b.
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+int add(int a, int b)
+
+{
+
+	return (a + b);
+
+}
+
+
