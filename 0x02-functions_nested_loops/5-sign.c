dc681b725c06d9cfd14fe750b34001900c238aef
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-05 18:59:10 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,21 +13,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	return (1);
 	_putchar (43);
-	_putchar (49);
+	return (1);
 	}
 	else if (n < 0)
 		{
-		return (-1);
 		_putchar (45);
-		_putchar (49);
+		return (-1);
 		}
 		else
 		{
+			_putchar(48);
 			return (0);
-			_putchar(48);
-			_putchar(48);
 		}
 }
 
---
+++ 0x02-functions_nested_loops/nuevo.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+*print_alphabet_x10-  prints 10 times the alphabet
+*print_alphabet - print once
+*/
+
+void print_alphabet_x10(void)
+{
+
+	int i;
+
+	for (i = 1 ; i <= 10 ; i++)
+		{
+		print_alphabet();
+		}
+	_putchar('\n');
+
+}
