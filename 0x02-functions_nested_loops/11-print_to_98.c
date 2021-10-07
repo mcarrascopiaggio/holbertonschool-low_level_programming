b74979a6978004425d769867a26f1217cbd49fc8
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-06 18:31:24 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,17 +13,16 @@ void print_to_98(int n)
 	{
 		for ( ; n > 98 ; n--)
 	{
-		printf("%d','_' '", n);
+		printf("%d, ", n);
 	}
 	}
 	else
 	{
 		for ( ; n < 98 ; n++)
 		{
-		printf("%d','_' '", n);
+		printf("%d, ", n);
 		}
 
 	}
-	printf("%d", 98);
-	_putchar('\n');
+	printf("%d\n", 98);
 }
