c596610fb50418bddfd6353505a6a2431f47fcdc
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-07 10:41:36 -0700

    upload 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,9 @@
 
 
 /**
-*_isdigit-  check if a variable is a digit
-*@c: The integer to print
+*_isdigit(int c)-  check if a variable is a digit
+*@c: The integer to check
+
 *Return: On success 1.
 *On error, -1 is returned, and errno is set appropriately.
 */
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _isupper(int c);
 int _putchar(char c);
 int _isdigit(int c);
@@ -10,3 +15,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 
+#endif /* MAIN_H */
