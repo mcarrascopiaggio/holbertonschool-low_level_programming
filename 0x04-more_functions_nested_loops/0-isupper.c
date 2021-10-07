ade1213ed8165fabf283c8f7045b83cbc7c5c2e8
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-07 11:38:23 -0700

    upload with betty correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,12 +4,11 @@
 /**
 *_upper(int c)-  check if a variable is a upper
 *@c: The integer to check
-
 *Return: On success 1.
 *On error, -1 is returned, and errno is set appropriately.
 */
 
-int _isdigit(int c)
+int _upper(int c)
 
 {
 	if (c >= 65  && c <= 90)
