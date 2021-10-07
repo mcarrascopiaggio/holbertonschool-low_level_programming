aab46ac82404b7802bfd5dc6b413be644ab48918
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-07 12:09:45 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%d\n", mul(1, 3));
+	printf("%d\n", mul(-4, 1));
+	return (0);
+}
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -12,6 +12,5 @@ int mul(int a, int b)
 	int result;
 
 	result = a * b;
-	_putchar(result + '0');
 	return (0);
 }
