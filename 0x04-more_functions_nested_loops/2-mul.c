886b1d8a8bf71b0840ea2a37773e07e30c49fb39
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-07 11:57:32 -0700

    upload content of task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *mul-  prints times_tablede multiplication from two values
+ *@a: the first value
+ *@b: the second value
+ *Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	_putchar(result + '0');
+	return (0);
+}
