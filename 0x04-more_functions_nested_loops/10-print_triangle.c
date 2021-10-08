136bcb8aaa77cddff30a27fc55fdc9a4546a8a08
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-07 19:39:32 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_triangle-  prints a triangulo
+ *@size: number of lines
+ *Return: always 0
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+	for (i = 1 ; i <= size ; i++)
+		{
+		for (j = 1 ; j <= i ; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+		}
+	}
+	else
+		{
+		_putchar('\n');
+}
+}
