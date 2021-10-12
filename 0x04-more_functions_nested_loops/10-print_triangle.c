4abc330063d532e3d970762f753ddf6e07428719
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-12 08:33:48 -0700

    projec empty files

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,9 +13,13 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-	for (i = 1 ; i <= size ; i++)
+	for (i = 1 ; i <= size  ; i++)
 		{
-		for (j = size ; j >= i ; j--)
+		for (j = 1 ; j < size ; j++)
+		{
+			_putchar(32);
+		}
+		for (j = 1; j - i <= size ; j++)
 		{
 			_putchar(35);
 		}
