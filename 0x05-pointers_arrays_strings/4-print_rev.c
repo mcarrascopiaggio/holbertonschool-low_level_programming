1b5d7a8c44adfb7eb23cba367a1d9c50d1b4ecfa
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-12 14:33:07 -0700

    upload content of e 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_rev - print reverse
+*@s: parametrer one
+*Return: Always 0.
+*/
+
+
+void print_rev(char *s)
+{
+
+int i;
+int l;
+int j;
+
+l = 0;
+
+for (i = 0; s[i] != 0 ; i++)
+	{
+	l = l + 1;
+	}
+for (j = (l - 1) ; s[j] >= 0 ; j--)
+	{
+	_putchar(s[j]);
+	}
+	_putchar(10);
+}
