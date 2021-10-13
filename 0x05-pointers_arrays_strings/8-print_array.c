64b18f4f7cb2c1b33d8594ca63ba02da6802ebce
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-12 19:42:52 -0700

    task eight

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_array - print reverse
+*@a: parametrer one
+*@n: second parameter
+*Return: Always 0.
+*/
+
+
+void print_array(int *a, int n)
+{
+
+int i;
+
+for (i = 0; i < (n - 1) ; i++)
+	{
+	printf("%d, ", a[i]);
+	}
+printf("%d\n", a[(n - 1)]);
+}
