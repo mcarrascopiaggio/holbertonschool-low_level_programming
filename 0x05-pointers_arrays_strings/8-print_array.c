0a345ab92d9124692eba7d4b7ee214f703fd951c
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-12 19:48:11 -0700

    task eight

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,17 @@ void print_array(int *a, int n)
 
 int i;
 
+
+if (n >= 1)
+{
 for (i = 0; i < (n - 1) ; i++)
 	{
 	printf("%d, ", a[i]);
 	}
 printf("%d\n", a[(n - 1)]);
+}
+else
+{
+printf("%d\n", a[(n - 1)]);
+}
 }
