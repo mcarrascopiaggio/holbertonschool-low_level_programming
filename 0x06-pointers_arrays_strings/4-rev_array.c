36f44caadb17504ec162bd051a4cd4940d3c3175
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-15 09:02:58 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*reverse_array - print reverse
+*@a: an array of integers
+*@n: the number of elements to swap
+*Return: Always 0.
+*/
+
+
+void reverse_array(int *a, int n)
+{
+int temp;
+int j;
+
+temp = 0;
+
+for (j = 0 ; j < n - j ; j++)
+	{
+	temp = a[n];
+	a[n] = a[j];
+	a[j] = temp;
+	}
+
+}
