ca42517637660e1863183976d231d32335ffedd4
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-15 20:19:35 -0700

    task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*rot13 -  encodes a string using rot13.
+*@src: character source
+*Return: Always 0.
+*/
+
+
+char *rot13(char *src)
+{
+
+int i;
+i = 0;
+
+for (i = 0; src[i] != '\0'; i++)
+{
+	if ((src[i] >= 65 && src[i] <= 77) || (src[i] >= 97 && src[i] <= 122))
+	{
+		src[i] = src[i] + 13;
+	}
+	else
+	{
+		src[i] = src[i] - 13;
+	}
+}
+return (src);
+}
