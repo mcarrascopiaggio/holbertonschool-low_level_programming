1bd2f657ab0a0a9a41289205a93630820c76c156
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-16 19:41:28 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*rot13 -  encodes a string using rot13.
+*rot13 - swaps values
 *@src: character source
 *Return: Always 0.
 */
@@ -11,25 +11,20 @@ char *rot13(char *src)
 {
 
 int i;
-
+int j;
+char I[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char O[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 i = 0;
+j = 0;
 
-for (i = 0; src[i] != 0; i++)
+for (i = 0; src[i] != '\0'; i++)
 {
-	if ((src[i] >= 65 && src[i] <= 77) || (src[i] >= 97 && src[i] <= 109))
+	for (j = 0; j < 52; j++)
 	{
-		src[i] = src[i] + 13;
-	}
-	else
-	{
-	if ((src[i] >= 78 && src[i] <= 90) || (src[i] >= 110 && src[i] <= 122))
-	{
-	src[i] = src[i] - 13;
-	}
-	else
-	{
-	src[i] = src[i];
-	}
+		if (src[i] == I[j])
+		{
+			src[i] = O[j];
+		}
 	}
 }
 return (src);
