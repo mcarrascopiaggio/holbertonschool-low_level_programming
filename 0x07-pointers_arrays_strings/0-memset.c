1f1c60a9798dec63ee24ed191d096b0c09e9bf62
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-18 10:31:23 -0700

    readme

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,16 +3,16 @@
 /**
 *_memset - prints buffer in hexa.
 *@s: the address of memory to print.
-*@n: the number of elements to swap.
+*@n: the size of the memory to print.
 *@b: the constant value.
-* Return: Always 0.
+*Return: Always 0.
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 	s[i] = b;
 	}
