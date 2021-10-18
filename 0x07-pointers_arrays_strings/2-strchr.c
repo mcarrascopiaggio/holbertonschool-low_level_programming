0a32c48e6ea712a3199eb42034e09de77c0380a0
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-18 11:55:13 -0700

    task 2 with a new case

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,18 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	int n;
 
 	i = 0;
-	n = 0;
 
 	for (i = 0; s[i] != c && s[i] != 0; i++)
 	{
-	n++;
 	}
-
-return (s + n);
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+	else
+	{
+		return ('\0');
+	}
 }
