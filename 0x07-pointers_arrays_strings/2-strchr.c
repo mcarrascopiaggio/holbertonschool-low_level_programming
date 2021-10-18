1289f30445b89f951687948150f2eecb0092a493
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-18 11:40:00 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_strchr - print the character where c appear..
+*@s: the address of memory to new str.
+*@c: the matching character.
+*Return: Always 0.
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	int n;
+
+	i = 0;
+	n = 0;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	n++;
+	if (s[i] == c)
+	{
+		break;
+	}
+	}
+
+return (s + n - 1);
+}
