15b0dd98cea5c966adfda9a55491e645129d2272
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-18 19:43:41 -0700

    task advance

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*set_string- prints buffer in hexa.
+*@s: the address of memory to print.
+*@to: the size of the memory to print.
+*Return: Always 0.
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
