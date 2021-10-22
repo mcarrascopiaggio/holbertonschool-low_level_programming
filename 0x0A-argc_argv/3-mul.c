81e3770127f3497afb87092c925cace91fc86394
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-22 08:26:03 -0700

    task 3 upload

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,12 +14,17 @@ int main(int argc, char *argv[])
 {
 int i, mul = 1;
 
-if (argc > 1)
-
+if (argc == 3)
+{
 for (i = 1; i < argc; i++)
 {
 mul = mul *atoi(argv[i]);
 }
 printf("%d\n", mul);
+}
+else
+{
+printf("Error\n");
+}
 	return (0);
 }
