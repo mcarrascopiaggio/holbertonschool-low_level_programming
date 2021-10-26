04bf03f45becee35103c38000ec9bca5a9b97095
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-26 15:09:18 -0700

    dos

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,39 +10,39 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-int i, j, l, m = 0;
+int i, j, k, count = 0;
 char *s;
 
 if (s1 == NULL)
 {
-	s1 = " ";
+	s1 = "";
 }
 if (s2 == NULL)
 {
-	s2 = " ";
+	s2 = "";
 }
 
 for (i = 0; s1[i] != 0; i++)
 {
-	i++;
+	;
 }
 for (j = 0; s2[j] != 0; j++)
 {
-	j++;
+	;
 }
-l = i + j;
-s = malloc((sizeof(char) * l) +1);
+k = i + j;
+s = (char *)malloc(k + 1);
 if (s == NULL)
 {
 	return (NULL);
 }
-for (m = 0; m < i; m++)
+for (count = 0; count < i; count++)
 {
-	s[m] = s1[m];
+	s[count] = s1[count];
 }
-for (m = i, j = 0; m < l; m++, j++)
+for (count = i, j = 0; count < k; count++, j++)
 {
-	s[m] = s2[j];
+	s[count] = s2[j];
 }
 return (s);
 }
