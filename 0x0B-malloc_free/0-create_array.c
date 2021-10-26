9541503630e589026de9c4c911e4e773c8e9997f
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-26 09:45:29 -0700

    lf[A

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *create_array - creates an array of chars, and init it with a specific char.
+ *@size: the size of the memory to printthe character to print
+ *@c: character to print
+ *Return: array of a character.
+ */
+char *create_array(unsigned int size, char c)
+{
+char *str;
+unsigned int i;
+
+if (size == 0)
+{
+return (NULL);
+}
+
+str = malloc(sizeof(char) * size * c);
+if (str == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+	str[i] = c;
+}
+return (str);
+}
