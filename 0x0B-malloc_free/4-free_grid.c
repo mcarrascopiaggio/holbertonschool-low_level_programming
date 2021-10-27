a85b3dafbddc3e3c053c23f74e986a39c29a4bc2
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-27 08:44:09 -0700

    task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,12 +13,9 @@ void free_grid(int **grid, int height)
 {
 int i = 0;
 
-if (grid[i] == 0)
+for (i = 0 ; i < height; i++)
 {
-	for (i = 0 ; i < height; i++)
-	{
-		free(grid[i]);
-	}
-free(grid);
+	free(grid[i]);
 }
+free(grid);
 }
