7a8966b207e72a46306605f2675063781843c485
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-10-27 08:38:20 -0700

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*free_grid - print a two dimensial array
+*@grid: base array
+*@height: height of array
+*Return: a pointer to a pointer
+*/
+
+void free_grid(int **grid, int height)
+{
+int i = 0;
+
+if (grid[i] == 0)
+{
+	for (i = height ; i >= 0; i--)
+	{
+		free(grid[i]);
+	}
+free(grid);
+}
+}
