6ba44fc5675f59e29e36525db5536ccdd23ac83b
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-11-01 08:51:47 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+*init_dog - function for initialize.
+*@d: name of structur
+*@name: name of the dog.
+*@age: age in month.
+*@owner: owner of the dog.
+*Return:
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
