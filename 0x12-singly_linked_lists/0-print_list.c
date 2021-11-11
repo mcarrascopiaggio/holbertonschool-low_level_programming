68aaba8ed849cd7d08747a1b6fe868ec25f4f44f
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-11-11 09:38:58 -0800

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,33 @@
-:
+#include "lists.h"
+
+/**
+*print_list - prints all the elements of a list_t list.
+*@h: pointer to a list_h
+*Return:the number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+size_t cnodes; /**counter of nodes*/
+
+cnodes = 0;
+
+if (h == NULL) /**if the structed pointed does not exist*/
+	return (0);
+else
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+		printf("[%d] %s", h->len, h->str);
+		}
+		cnodes++;
+		h = h->next; /** way to move from node to node */
+	}
+return (cnodes);
+
+}
