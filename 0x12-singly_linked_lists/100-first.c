851821852d2f2b0a18e0cb758ed94281c24ae4b3
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-11-11 17:36:47 -0800

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+*constructor - Apply the constructor attribut to executed before main()
+*/
+
+void constructor(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
