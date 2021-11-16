cf60893eb12072c6f4e78352266926a558799077
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-11-15 18:12:13 -0800

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,11 @@ void free_listint2(listint_t **head)
 {
 listint_t *aux;
 
-while (*head)
-{
-aux = *head;
-aux = aux->next;
-free(aux);
-}
-*head = NULL;
+	while (head)
+	{
+		aux = *head;
+		*head = aux->next;
+		free(aux);
+	}
+	*head = NULL;
 }
