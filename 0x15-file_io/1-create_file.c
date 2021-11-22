480c1c0486aa8fccc33ffdfabdc048325079d351
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-11-22 11:48:49 -0800

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,12 +10,23 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
+int w;
+int i;
+
+if (filename == NULL)
+	return (-1);
 
 fd = open(filename, O_RDONLY, 600);
 
 if (fd == -1)
 	return (-1);
-write(fd, text_content, 1);
+for (i = 0; text_content[i] != '\0'; i++)
+{
+}
+w = write(fd, text_content, i);
+if (w == -1)
+	return (-1);
+
 close(fd);
 return (fd);
 }
