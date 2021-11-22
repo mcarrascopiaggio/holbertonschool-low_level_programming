34e39e48546d1dfcc293050598c01019413ca8b4
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2021-11-22 11:35:14 -0800

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *create_file - function that creates a file
+ *@filename: is the name of the file to create.
+ *@text_content: NULL terminated string to write to the file
+ *Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+
+fd = open(filename, O_RDONLY, 600);
+
+if (fd == -1)
+	return (-1);
+write(fd, text_content, 1);
+close(fd);
+return (fd);
+}
