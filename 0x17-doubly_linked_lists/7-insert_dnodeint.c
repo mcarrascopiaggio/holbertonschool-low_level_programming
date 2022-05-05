22f1c8d67c3f7bca17e0f30ee9059145401d5369
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-05 11:55:26 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int count = dlistint_len(*h);
 	unsigned int i = 0;
 
-	if (!*h || !h || idx >= count)
+	if (!h || idx >= count)
 	{
 		return (NULL);
 	}
