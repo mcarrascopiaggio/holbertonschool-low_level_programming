29e65e9e897597882d5961d4168d358669837e5a
mcarrascopiaggio <mcarrascopiaggio@gmail.com>
2022-05-26 18:53:04 -0700

    still memory leaks

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_delete - function that deletes a hash table.
+ *@ht: is the hash table
+ *Return: nothing
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *temp;
+	hash_node_t *aux;
+	unsigned long int index = 0;
+
+	if (ht == NULL)
+		return;
+
+	while (index < ht->size)
+	{
+		temp = ht->array[index];
+		while (temp)
+		{
+			aux = temp;
+			free(temp->key);
+			free(temp->value);
+			temp = temp->next;
+			free(aux);
+		}
+		ht->array[index] = NULL;
+		index++;
+	}
+	free(ht->array);
+	free(ht);
+}
