3171001345e0b3dcc2d948471d3c7fdad470356a
root <mcarrascopiaggio@gmail.com>
2022-08-25 10:18:12 -0700

    betty & output ok

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,22 +11,23 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
-	size_t flag = 0;
+	size_t i;
 
 	if (array == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
 	{
-		if (array[i] == value)
-		{
-			flag = 1;
-			break;
-		}
-		i = i + 1;
-	}
-	if (flag == 1)
-		return (i);
-	else
 		return (-1);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
+		}
+		return (-1);
+	}
+
 }
